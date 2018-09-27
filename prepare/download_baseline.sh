1e0d14b7f38b51172baf24c98db953f9b02ff487
Mike Mayers <mmayers@scripps.edu>
2018-09-26 17:04:03 -0700

    update download scripts to output to proper directories

--- prepare/download_baseline.sh
+++ prepare/download_baseline.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-mkdir -p ../data/basedir
-cd ../data/basedir
+# Make the baseline directory for download
+mkdir -p ../data/baseline
+cd ../data/baseline
 
+# Download all the files
 wget -r ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/
+
+# Fix the directory sturcutre
 pushd ftp.ncbi.nlm.nih.gov/pubmed/baseline/
 cp * ../../../
 popd
 rm -rf ftp.ncbi.nlm.nih.gov
 
+# Make sure all the files are OK
 cat *.md5 > all.md5
-
 md5sum -c all.md5
--- prepare/download_semmeddb.sh
+++ prepare/download_semmeddb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Download the large file (about 1hr)
 wget https://skr3.nlm.nih.gov/SemMedDB/download/semmedVER31_R_PREDICATION_06302018.sql.gz -O ../data/semmedVER31_R_PREDICATION_06302018.sql.gz
 
+# Column names already provided in directory
 cp ../data/col_names.txt ../data/semmedVER31_R.csv
+# pv allows progress to be monitored, if pv is not installed can simply zcat ../semmedVER31_R... | python my_sq...
 pv ../data/semmedVER31_R_PREDICATION_06302018.sql.gz | zcat | python mysqldump_to_csv.py >> ../data/semmedVER31_R.csv
